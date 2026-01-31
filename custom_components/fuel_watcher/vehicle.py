3b29829c4576eb59b8fd3fb7cdd7655f29a7c311
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-01-31 20:11:50 +0100

    Create vehicle.py

---
+++ custom_components/fuel_watcher/vehicle.py
@@ -0,0 +1,16 @@
+def _read_state(hass, entity_id):
+    if not entity_id:
+        return None
+    state = hass.states.get(entity_id)
+    if not state or state.state in ("unknown", "unavailable"):
+        return None
+    return state.state
+
+def get_vehicle_data(hass, entry):
+    data = {
+        "fuel_level": _read_state(hass, entry.data.get("entity_fuel_level")),
+        "range": _read_state(hass, entry.data.get("entity_range")),
+        "consumption": _read_state(hass, entry.data.get("entity_consumption")),
+        "location": _read_state(hass, entry.data.get("entity_location")),
+    }
+    return data

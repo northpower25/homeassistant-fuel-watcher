dacf3adff211bca79895bf5165229eaa4bc58af0
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-01-31 20:04:44 +0100

    Create const.py

---
+++ custom_components/fuel_watcher/const.py
@@ -0,0 +1,18 @@
+DOMAIN = "fuel_watcher"
+
+CONF_TANKERKOENIG_API = "tankerkoenig_api_key"
+CONF_TELEGRAM_TOKEN = "telegram_token"
+CONF_TELEGRAM_CHAT_ID = "telegram_chat_id"
+CONF_PLZ = "plz"
+CONF_RADIUS = "radius"
+CONF_FUEL = "fuel"
+
+CONF_ENTITY_FUEL_LEVEL = "entity_fuel_level"
+CONF_ENTITY_RANGE = "entity_range"
+CONF_ENTITY_CONSUMPTION = "entity_consumption"
+CONF_ENTITY_LOCATION = "entity_location"
+
+CONF_PRICE_THRESHOLD = "price_threshold"
+CONF_DISTANCE_THRESHOLD = "distance_threshold_km"
+
+HISTORY_FILE = "/config/fuel_watcher_history.json"

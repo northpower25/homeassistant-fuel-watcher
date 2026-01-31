862ba5779b62efdbe583f441cbc53e4174753ba6
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-01-31 20:01:14 +0100

    Create __init__.py

---
+++ custom_components/fuel_watcher/__init__.py
@@ -0,0 +1,7 @@
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True

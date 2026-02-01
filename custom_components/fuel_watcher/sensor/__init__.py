974fc057b6688b05135ee54c80ceb37c3c254a1e
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 23:52:49 +0100

    Update __init__.py

--- custom_components/fuel_watcher/sensor/__init__.py
+++ custom_components/fuel_watcher/sensor/__init__.py
@@ -5,8 +5,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .main import FuelWatcherSensor
 from .diagnostics import FuelWatcherDiagnosticsSensor
 from .location import FuelWatcherLocationSensor
-
-# Derived Sensoren
 from .derived import (
     FuelWatcherPriceSensor,
     FuelWatcherStationNameSensor,
@@ -29,15 +27,10 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ):
     """Set up Fuel Watcher sensors."""
-
-    # Hauptsensor
     main_sensor = FuelWatcherSensor(hass, entry)
-
-    # Diagnose- und Standortsensoren
     diag_sensor = FuelWatcherDiagnosticsSensor(hass, entry, main_sensor)
     location_sensor = FuelWatcherLocationSensor(hass, entry)
 
-    # Derived Sensoren
     derived_sensors = [
         FuelWatcherPriceSensor(main_sensor),
         FuelWatcherStationNameSensor(main_sensor),
@@ -53,6 +46,4 @@ async def async_setup_entry(
         FuelWatcherExpectedConsumptionTomorrowSensor(main_sensor),
     ]
 
-    async_add_entities(
-        [main_sensor, diag_sensor, location_sensor] + derived_sensors
-    )
+    async_add_entities([main_sensor, diag_sensor, location_sensor] + derived_sensors)

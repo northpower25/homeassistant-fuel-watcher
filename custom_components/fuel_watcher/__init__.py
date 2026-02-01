f56abdea612fa12bf80c60238bd042d0770756ef
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 17:54:54 +0100

    Update __init__.py

--- custom_components/fuel_watcher/__init__.py
+++ custom_components/fuel_watcher/__init__.py
@@ -1,6 +1,5 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import async_load_platform
 
 from .const import DOMAIN
 
@@ -8,10 +7,6 @@ from .const import DOMAIN
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Fuel Watcher from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-
-    # Sensor-Plattform laden
-    await async_load_platform(hass, "sensor", DOMAIN, {}, entry)
-
     return True
 
 

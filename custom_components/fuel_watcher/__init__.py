632a58a689f63b57b8a73c7acfdfdd6314e80e1c
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 20:17:11 +0100

    Update __init__.py

--- custom_components/fuel_watcher/__init__.py
+++ custom_components/fuel_watcher/__init__.py
@@ -3,13 +3,14 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+PLATFORMS = ["sensor"]
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Fuel Watcher from a config entry."""
     hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload Fuel Watcher."""
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)

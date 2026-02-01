e28a77e44218dcdaa298a7dcaf912b56d8698990
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 15:56:43 +0100

    Create init.py

---
+++ custom_components/fuel_watcher/sensor/init.py
@@ -0,0 +1,8 @@
+from .main import FuelWatcherSensor
+from .diagnostics import FuelWatcherDiagnosticsSensor
+from .location import FuelWatcherLocationSensor
+from .postcode import FuelWatcherPostcodeSensor
+
+PLATFORMS = [
+    "sensor"
+]

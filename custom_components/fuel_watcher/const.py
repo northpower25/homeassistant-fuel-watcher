58d14b5eb048b356cfa749198f62aa710ae4e813
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 16:09:10 +0100

    Update const.py

--- custom_components/fuel_watcher/const.py
+++ custom_components/fuel_watcher/const.py
@@ -4,7 +4,6 @@ CONF_TANKERKOENIG_API = "tankerkoenig_api"
 CONF_TELEGRAM_TOKEN = "telegram_token"
 CONF_TELEGRAM_CHAT_ID = "telegram_chat_id"
 
-CONF_PLZ = "plz"
 CONF_RADIUS = "radius"
 CONF_FUEL = "fuel"
 CONF_SOURCE = "source"
@@ -18,10 +17,12 @@ CONF_ENTITY_CONSUMPTION = "entity_consumption"
 CONF_ENTITY_ODOMETER = "entity_odometer"
 CONF_ENTITY_LOCATION = "entity_location"
 
-CONF_DYNAMIC_PLZ = "dynamic_plz"
+CONF_DYNAMIC_PLZ = "dynamic_plz"   # bleibt für Debug/Anzeige
 
 SOURCE_TANKERKOENIG = "tankerkoenig"
 
 SUPPORTED_SOURCES = [
     SOURCE_TANKERKOENIG,
 ]
+
+HISTORY_FILE = "fuel_history.json"

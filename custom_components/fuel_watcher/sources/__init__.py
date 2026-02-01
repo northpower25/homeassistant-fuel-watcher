558301bd82c897b07a3af5014db4f890102c02c1
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-01 02:00:36 +0100

    Update __init__.py

--- custom_components/fuel_watcher/sources/__init__.py
+++ custom_components/fuel_watcher/sources/__init__.py
@@ -4,6 +4,6 @@ from ..const import SOURCE_TANKERKOENIG
 async def get_cheapest(hass, source, api_key, plz, radius, fuel):
     if source == SOURCE_TANKERKOENIG:
         return await get_cheapest_tankerkoenig(hass, api_key, plz, radius, fuel)
-    # Platz für weitere Quellen:
-    # elif source == "fuelprice_io": ...
+
     return None
+

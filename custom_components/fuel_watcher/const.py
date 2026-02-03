77b38b25519f5229b5a9e90ee975a6d7bdf72165
northpower25 <36472486+northpower25@users.noreply.github.com>
2026-02-03 18:18:33 +0100

    feat(const): add vehicle_name and vehicle entity keys (odometer, fuel level, location, consumption), Trasholds for decition, Notify messages

--- custom_components/fuel_watcher/const.py
+++ custom_components/fuel_watcher/const.py
@@ -1,6 +1,6 @@
 """
 Commit: feat(const): add vehicle_name and vehicle entity keys
-(odometer, fuel level, location, consumption), Trasholds for decition
+(odometer, fuel level, location, consumption), Trasholds for decition, Notify messages
 """
 
 DOMAIN = "fuel_watcher"
@@ -26,5 +26,25 @@ CONF_ENTITY_CONSUMPTION = "entity_consumption"
 # Trasholds for decition
 CONF_PRICE_DROP_PERCENT_THRESHOLD = "price_drop_percent_threshold"
 CONF_PRICE_DROP_ABSOLUTE_THRESHOLD = "price_drop_absolute_threshold"
+
+# Notify messages
+CONF_NOTIFY_MSG_TANKEN = "notify_msg_tanken"
+CONF_NOTIFY_MSG_RANGE_DAYS = "notify_msg_range_days"
+
+DEFAULT_NOTIFY_MSG_TANKEN = (
+    "⛽ *Tankempfehlung für {vehicle}*\n"
+    "Grund: _{reason}_\n"
+    "Preis: `{price} €/l`\n"
+    "Δ: `{delta} €/l` ({delta_percent} %)\n"
+    "Reichweite: `{range_km} km`\n"
+    "📍 {station} ({distance_km} km)\n"
+    "[Navigation](https://www.google.com/maps/search/?api=1&query={lat},{lng})"
+)
+
+DEFAULT_NOTIFY_MSG_RANGE_DAYS = (
+    "⚠️ *Reichweitenwarnung für {vehicle}*\n"
+    "Noch `{days_left}` Tage verbleibend.\n"
+    "Aktuelle Reichweite: `{range_km} km`"
+)
 
 PLATFORMS = ["sensor"]
